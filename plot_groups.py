509943ced24aa9c49a780c17f82eceadb7794eaf
W Hayden Fuss <wifu1234@gmail.com>
2015-05-15 13:55:02 -0400

    Touched up plot_groups.py

--- plot_groups.py
+++ plot_groups.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 current_dir = os.getcwd()
 
 # simulation variables
-block_lengths = np.array([ 10, 30, 50, 70, 90 ])
+block_lengths = np.array([ 30, 50, 70, 90 ])
 salt_concs = np.array([ 25, 30, 40, 50, 70, 90 ])
 
 measurements = {'cores': {}, 'agg': {}, 'dist': {}, 'stems': {}}
 
-colors = {10:'r', 30:'b', 50:'g', 70:'m', 90:'c'}
+colors = { 30:'b', 50:'g', 70:'m', 90:'c'}
 
 for i in measurements.keys():
   for l in block_lengths:
@@ -45,10 +45,15 @@ for l in block_lengths:
 
 # cores
 for l in colors.keys():
-  plt.errorbar(salt_concs, measurements['cores'][l]['mean'], yerr=measurements['cores'][l]['std'], marker='o', color=colors[l])
+  plt.errorbar(salt_concs, measurements['cores'][l]['mean'], yerr=measurements['cores'][l]['std'], marker='o', color=colors[l], label=str(l))
 
 plt.ylabel('# of Cores')
 plt.xlabel('$ a_{2,2} $')
+plt.legend()
+
+x1, x2, y1, y2 = plt.axis()
+
+plt.axis([0, 100, y1, y2])
 
 plt.savefig("cores.png", dpi=96)
 
@@ -56,10 +61,15 @@ plt.clf()
 
 # agg
 for l in colors.keys():
-  plt.errorbar(salt_concs, measurements['agg'][l]['mean'], yerr=measurements['agg'][l]['std'], marker='o', color=colors[l])
+  plt.errorbar(salt_concs, measurements['agg'][l]['mean'], yerr=measurements['agg'][l]['std'], marker='o', color=colors[l], label=str(l))
 
 plt.ylabel('Avg. Aggregation Number / Core')
 plt.xlabel('$ a_{2,2} $')
+plt.legend()
+
+x1, x2, y1, y2 = plt.axis()
+
+plt.axis([0, 100, y1, y2])
 
 plt.savefig("agg.png", dpi=96)
 
@@ -67,10 +77,15 @@ plt.clf()
 
 # dist
 for l in colors.keys():
-  plt.errorbar(salt_concs, measurements['dist'][l]['mean'], yerr=measurements['dist'][l]['std'], marker='o', color=colors[l])
+  plt.errorbar(salt_concs, measurements['dist'][l]['mean'], yerr=measurements['dist'][l]['std'], marker='o', color=colors[l], label=str(l))
 
 plt.ylabel('Avg. Distance Between Linked Cores')
 plt.xlabel('$ a_{2,2} $')
+plt.legend()
+
+x1, x2, y1, y2 = plt.axis()
+
+plt.axis([0, 100, y1, y2])
 
 plt.savefig("dist.png", dpi=96)
 
@@ -78,10 +93,15 @@ plt.clf()
 
 # stems
 for l in colors.keys():
-  plt.errorbar(salt_concs, measurements['stems'][l]['mean'], yerr=measurements['stems'][l]['std'], marker='o', color=colors[l])
+  plt.errorbar(salt_concs, measurements['stems'][l]['mean'], yerr=measurements['stems'][l]['std'], marker='o', color=colors[l], label=str(l))
 
 plt.ylabel('Percentage of Stems')
 plt.xlabel('$ a_{2,2} $')
+plt.legend()
+
+x1, x2, y1, y2 = plt.axis()
+
+plt.axis([0, 100, y1, y2])
 
 plt.savefig("stems.png", dpi=96)
 
