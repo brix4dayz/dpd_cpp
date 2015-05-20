7decd908882c216cdfb5b74c575d7967cc9c6e1f
W Hayden Fuss <wifu1234@gmail.com>
2015-05-20 12:56:28 -0400

    Reorganized scripts and made time plotter for main groups.

--- scripts/triblock/plotters/plot_time_evol.py
+++ scripts/triblock/plotters/plot_time_evol.py
@@ -4,70 +4,70 @@ import numpy as np
 import matplotlib.pyplot as plt
 import operator
 
-Na = 30
-a22 = 50
-
 # location of where we are
 current_dir = os.getcwd()
 
-measurements = {'t': { 'idx':0 }, 'cores': { 'idx':1 }, 'agg': { 'idx':2 }, 'dist': { 'idx':3 }, 'stems': { 'idx':4 }}
+# simulation variables
+block_lengths = np.array([ 30, 50, 70, 90 ])
+salt_concs = np.array([ 25, 30, 40, 50, 70, 90 ])
 
-delta = {'33%':'g', '67%':'b', '100%':'m'}
+for Na in block_lengths:
 
-labels = { '33%': '33%', '67%': '67%', '100%': '99%'}
+  measurements = {'t': { 'idx':0 }, 'cores': { 'idx':1 }, 'agg': { 'idx':2 }, 'dist': { 'idx':3 }, 'stems': { 'idx':4 }}
 
-titles = {'cores': '# of Cores', 'agg': 'Avg. Aggregation Number / Core', 'dist': 'Avg. Distance Between Linked Cores', 'stems': 'Percentage of Stem Chains'}
+  saltColors = { 25:'b', 30:'g', 40:'r', 50:'c', 70:'m', 90:'y'}
 
-for i in measurements.keys():
-  for d in delta.keys():
-    measurements[i][d] = np.array([])
+  titles = {'cores': '# of Cores', 'agg': 'Avg. Aggregation Number / Core', 'dist': 'Avg. Distance Between Linked Cores', 'stems': 'Percentage of Stem Chains'}
 
-for d in delta:
-   # relative path to folder
-  os.chdir(d) # go to group folder
-  print(os.getcwd()) # print which group we're in
+  for i in measurements.keys():
+    for a22 in salt_concs:
+      measurements[i][a22] = np.array([])
 
-  f = open( str(Na) + "_" + str(a22) + "_time_evol.dat", "r")
+  for a22 in salt_concs:
+    # relative path to folder
+    folder = str(Na) + "_" + str(a22)
+    os.chdir(folder) # go to group folder
+    print(os.getcwd()) # print which group we're in
 
-  for line in f:
-    tokens = line.split()
-    if tokens[0] != "Frame":
-      for m in measurements.keys():
-        measurements[m][d] = np.append(measurements[m][d], [float(tokens[measurements[m]['idx']])])
+    f = open( folder + "_time_evol.dat", "r")
 
-  f.close()
+    for line in f:
+      tokens = line.split()
+      if tokens[0] != "Frame":
+        for m in measurements.keys():
+          measurements[m][a22] = np.append(measurements[m][a22], [float(tokens[measurements[m]['idx']])])
 
-  os.chdir(current_dir)
+    f.close()
 
-# make plots
-for i in measurements.keys():
-  if i != 't':
-    # plot each length for the current measurement
-    for d in delta.keys():
-      plt.plot(measurements['t'][d]*10, measurements[i][d], 
-               color=delta[d], label=labels[d], linewidth=2.0)
+    os.chdir(current_dir)
 
-    plt.ylabel(titles[i])
-    plt.xlabel('$ \\tau $', fontsize=25)
+  # make plots
+  for i in measurements.keys():
+    if i != 't':
+      # plot each length for the current measurement
+      for a22 in salt_concs:
+        plt.plot(measurements['t'][a22]*10, measurements[i][a22], 
+                 color=saltColors[a22], label=str(a22), linewidth=2.0)
 
-    # sort legend and put in top right corner
-    if i == 'agg':
+      plt.ylabel(titles[i])
+      plt.xlabel('$ \\tau $', fontsize=25)
+
+      # sort legend and put in top right corner
       ax = plt.gca()
       handles, labels = ax.get_legend_handles_labels()
       hl = sorted(zip(handles, labels), key=operator.itemgetter(1))
       handles2, labels2 = zip(*hl)
-      labels2[2] = '100%'
       ax.legend(handles2, labels2, bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
 
-    x1, x2, y1, y2 = plt.axis()
+      x1, x2, y1, y2 = plt.axis()
 
-    if i != 'cores':
-      plt.axis([0, 6000, y1, y2])
-    else:
-      plt.axis([0, 6000, y1, 40])
+      if i != 'cores':
+        plt.axis([0, 6000, y1, y2])
+      else:
+        plt.axis([0, 6000, y1, 40])
 
-    plt.savefig( i + "_" + str(Na) + "_" + str(a22) + "_time.png", dpi=96)
+      plt.savefig( i + "_" + str(Na) + "_time.png", dpi=96)
 
-    plt.clf()
+      plt.clf()
 
 
---
+++ scripts/triblock/vary_charge/plotters/vc_plot_time.py
@@ -0,0 +1,78 @@
+import subprocess
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import operator
+
+Na = 30
+a22 = 50
+
+# location of where we are
+current_dir = os.getcwd()
+
+measurements = {'t': { 'idx':0 }, 'cores': { 'idx':1 }, 'agg': { 'idx':2 }, 'dist': { 'idx':3 }, 'stems': { 'idx':4 }}
+
+delta = {'33%':'g', '67%':'b', '100%':'m'}
+
+labs = { '33%': '33%', '67%': '67%', '100%': '99%'}
+
+titles = {'cores': '# of Cores', 'agg': 'Avg. Aggregation Number / Core', 'dist': 'Avg. Distance Between Linked Cores', 'stems': 'Percentage of Stem Chains'}
+
+for i in measurements.keys():
+  for d in delta.keys():
+    measurements[i][d] = np.array([])
+
+for d in delta:
+   # relative path to folder
+  os.chdir(d) # go to group folder
+  print(os.getcwd()) # print which group we're in
+
+  f = open( str(Na) + "_" + str(a22) + "_time_evol.dat", "r")
+
+  for line in f:
+    tokens = line.split()
+    if tokens[0] != "Frame":
+      for m in measurements.keys():
+        measurements[m][d] = np.append(measurements[m][d], [float(tokens[measurements[m]['idx']])])
+
+  f.close()
+
+  os.chdir(current_dir)
+
+# make plots
+for i in measurements.keys():
+  if i != 't':
+    # plot each length for the current measurement
+    for d in delta.keys():
+      plt.plot(measurements['t'][d]*10, measurements[i][d], 
+               color=delta[d], label=labs[d], linewidth=2.0)
+
+    plt.ylabel(titles[i])
+    plt.xlabel('$ \\tau $', fontsize=25)
+
+    # sort legend and put in top right corner
+    if i == 'agg':
+      ax = plt.gca()
+      handles, labels = ax.get_legend_handles_labels()
+      hl = sorted(zip(handles, labels), key=operator.itemgetter(1))
+      handles2, labels2 = zip(*hl)
+      # the sorting would put 100% at the top, so I mapped it to 99%
+      # then replace the 99% label with 100%, an ugly hack but
+      # it gets the job done
+      labels2 = list(labels2)
+      labels2[2] = '100%'
+      labels2 = tuple(labels2)
+      ax.legend(handles2, labels2, bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
+
+    x1, x2, y1, y2 = plt.axis()
+
+    if i != 'cores':
+      plt.axis([0, 6000, y1, y2])
+    else:
+      plt.axis([0, 6000, y1, 40])
+
+    plt.savefig( i + "_" + str(Na) + "_" + str(a22) + "_time.png", dpi=96)
+
+    plt.clf()
+
+
