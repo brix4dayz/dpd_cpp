4087e414cacb4351eb3f7e648e9b8cc3b9c9dafd
W Hayden Fuss <wifu1234@gmail.com>
2015-05-22 13:17:19 -0400

    Fixed mistake in setup_path.

--- dpd_cpp/setup_dpd_cpp_path.sh
+++ dpd_cpp/setup_dpd_cpp_path.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [[ "$PATH" == *":/gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd_cpp/bin/:"* ]] 
+if [[ "$PATH" == *":/gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd_cpp/bin/"* ]] 
 then
   echo "Path already up-to-date.";
 else
   echo "set path=(\$path /gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd_cpp/bin/)" >> ~/.cshrc;
 fi
 
-if [[ "$LD_LIBRARY_PATH" == *":/usr/local/apps/gcc/473/lib64/:"* ]]
+if [[ "$LD_LIBRARY_PATH" == *":/usr/local/apps/gcc/473/lib64/"* ]]
 then
   echo "LD_LIBRARY_PATH already up-to-date.";
 else
-  echo "setenv LD_LIBRARY_PATH '\${LD_LIBRARY_PATH}:/usr/local/apps/gcc/473/lib64/'" >> ~/.cshrc;
+  echo "setenv LD_LIBRARY_PATH \${LD_LIBRARY_PATH}:/usr/local/apps/gcc/473/lib64/" >> ~/.cshrc;
 fi
