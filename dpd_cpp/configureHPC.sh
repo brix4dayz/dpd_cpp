4318ad6356c83d0c2e2b79e752ed294fb2f1c183
W Hayden Fuss <wifu1234@gmail.com>
2015-11-23 12:37:15 -0500

    Adjusted scripts for configuring environment and processing vc simulations.

--- dpd_cpp/configureHPC.sh
+++ dpd_cpp/configureHPC.sh
@@ -8,12 +8,12 @@ else
   echo "set path=(\$path /gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd_cpp/bin/)" >> ~/.cshrc;
 fi
 
-if [[ "$LD_LIBRARY_PATH" == *":/usr/local/apps/gcc/473/lib64/"* ]]
+if [[ "$LD_LIBRARY_PATH" == *":/usr/local/apps/gcc/484/lib64/"* ]]
 then
   echo "LD_LIBRARY_PATH already up-to-date.";
 else
   echo "You need to run: \`source ~/.cshrc\` when this is done running.";
-  echo "setenv LD_LIBRARY_PATH \${LD_LIBRARY_PATH}:/usr/local/apps/gcc/473/lib64/" >> ~/.cshrc;
+  echo "setenv LD_LIBRARY_PATH \${LD_LIBRARY_PATH}:/usr/local/apps/gcc/484/lib64/" >> ~/.cshrc;
 fi
 
 if [[ "$LD_LIBRARY_PATH" == *":/gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd_cpp/lib/"* ]]
