fefdaadcb75d4e601b80dd1742dfb41ca67860d5
W Hayden Fuss <wifu1234@gmail.com>
2016-03-02 12:34:27 -0500

    Updated configure script.

--- dpd_cpp/configureHPC.sh
+++ dpd_cpp/configureHPC.sh
@@ -23,3 +23,27 @@ else
   echo "You need to run: \`source ~/.cshrc\` when this is done running.";
   echo "setenv LD_LIBRARY_PATH \${LD_LIBRARY_PATH}:/gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd_cpp/lib/" >> ~/.cshrc;
 fi
+
+if [[ "$PATH" == *":/gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd2/"* ]] 
+then
+  echo "Path already up-to-date.";
+else
+  echo "You need to run: \`source ~/.cshrc\` when this is done running.";
+  echo "set path=(\$path /gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd2/)" >> ~/.cshrc;
+fi
+
+if [[ "$PATH" == *":/gpfs_partners/yingling/backup/Fuss/dpd_cpp/testdpd2/src/"* ]] 
+then
+  echo "Path already up-to-date.";
+else
+  echo "You need to run: \`source ~/.cshrc\` when this is done running.";
+  echo "set path=(\$path /gpfs_partners/yingling/backup/Fuss/dpd_cpp/testdpd2/src/)" >> ~/.cshrc;
+fi
+
+if [[ "$LD_LIBRARY_PATH" == *":/gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd2/lib/"* ]]
+then
+  echo "LD_LIBRARY_PATH already up-to-date.";
+else
+  echo "You need to run: \`source ~/.cshrc\` when this is done running.";
+  echo "setenv LD_LIBRARY_PATH \${LD_LIBRARY_PATH}:/gpfs_partners/yingling/backup/Fuss/dpd_cpp/dpd2/lib/" >> ~/.cshrc;
+fi
