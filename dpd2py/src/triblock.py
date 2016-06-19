b80406c4b6b487f92d401587f8cdf5f9c4f3168d
W Hayden Fuss <wifu1234@gmail.com>
2016-06-19 19:05:45 -0400

    Outlined the main for triblock py script.

--- dpd2py/src/triblock.py
+++ dpd2py/src/triblock.py
@@ -1,10 +1,15 @@
 from dpd2 import SimulationObject, BinBox, freeObjList
 from sys import argv
 
-
-
 def bead(line):
-    pass
+    
+    data = line.split()
+    _bead = SimulationObject(x=data[1], 
+                             y=data[2],
+                             z=data[3])
+    
+    setattr(_bead, "bead_id", data[0])    
+    return _bead
 
 def hydrophobe_tail(num_beads, in_file):
     _tail = {}
@@ -13,9 +18,11 @@ def hydrophobe_tail(num_beads, in_file):
     # TODO calc center of mass, store reference to other tail
     return _tail
     
-
 def polymer_block(num_beads, in_file):
     _block = []
+    
+    for i in range(num_beads):
+        _block.append(bead(in_file.readline()))
     
     return _block
 
@@ -26,6 +33,7 @@ def triblock(poly_length, tail_length, in_file):
        "tail2": hydrophobe_tail(tail_length, in_file)
     }
     
+    # TODO this just be center of mass beads for tails
     def get_beads():
         _beads = []
         
@@ -41,7 +49,6 @@ def triblock(poly_length, tail_length, in_file):
     return _triblock
 
 def frame(num_chains, poly_length, tail_length, in_file):
-    # TODO
     _frame = {
        "chains": [],
        "beads": [],
@@ -59,7 +66,37 @@ def frame(num_chains, poly_length, tail_length, in_file):
 def micelle(cluster):
     pass
 
-if __name__ == "__main__":
+def process(**kwargs):
     
+    num_beads_per_frame = kwargs["num_beads"]
+    poly_length = kwargs["poly_length"]
+    tail_length = kwargs["tail_length"]
+    
+    # TODO come up with a clever way to determine
+    # the number of frames without have to reading 
+    # the whole file
+    num_frames = kwargs["num_frames"]
+    chain_length = poly_length + 2 * tail_length
+    num_chains = num_beads_per_frame // chain_length
+    
+    cluster_solver = BinBox(boxDimensions=kwargs["box_dimensions"])
+    
+    with open(kwargs["input_file"], "r") as in_file:
+        for i in range(num_frames):            
+            _frame = frame(num_chains, poly_length, tail_length, in_file)
+            
+            cluster_solver.deriveClusters(_frame["beads"])
+            
+            freeObjList(_frame["beads"])
+            
+            _frame["micelles"] = []
+            for cluster in cluster_solver.clusterList:
+                _frame["micelles"].append(micelle(cluster))
+                
+            cluster_solver.empty()
+
+
+if __name__ == "__main__":
     pass
+
     
